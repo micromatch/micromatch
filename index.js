34cefb32166ec13016e30fee65e2bce695840672
jonschlinkert <jon.schlinkert@sellside.com>
2014-12-23 00:33:08 -0500

    add caching, start adding match functions

--- index.js
+++ index.js
@@ -7,10 +7,22 @@
 
 'use strict';
 
+var unixify = require('unixify');
+var union = require('arr-union');
+var diff = require('arr-diff');
 var braces = require('braces');
 
-var dotfile = '[^\\/]*?';
+/**
+ * Special patterns
+ */
+
 var matchBase = '[\\s\\S]+';
+var dotfile = '[^\\/]*?';
+
+/**
+ * Glob tokens to match and replace with
+ * regular expressions
+ */
 
 var tokens = [
   ['\\\\', {re: /\\{2}/g, to: '\\/'}],
@@ -21,19 +33,42 @@ var tokens = [
   ['*',    {re: /[*]/g,  to: '[^\\/]*?', matchBase: matchBase, dot: dotfile}],
 ];
 
-module.exports.makeRe = function makeRe(str, options) {
-  var opts = options || {};
-  var len = tokens.length;
-  var i = 0;
-  var f = '';
+var len = tokens.length;
 
-  if (/\{/.test(str)) {
-    str = braces(str).join('|');
+function makeRe(glob, options) {
+  glob = unixify(glob);
+  var opts = options || {};
+  var flags = '';
+  var i = 0;
+
+  optsCache = optsCache || opts;
+  if (!equal(optsCache, opts)) {
+    regex = null;
+    cache = glob;
+  }
+
+  cache = cache || glob;
+  if (cache !== glob) {
+    regex = null;
+    cache = glob;
+  }
+
+  if (regex instanceof RegExp) {
+    return regex;
+  }
+
+  if (/\{/.test(glob)) {
+    if (!/\{.*\{/.test(glob)) {
+      glob = glob.replace(/\{/g, '(');
+      glob = glob.replace(/,/g, '|');
+      glob = glob.replace(/\}/g, ')');
+    } else {
+      glob = braces(glob).join('|');
+    }
   }
 
   while (i < len) {
     var group = tokens[i++];
-    var token = group[0];
     var re = group[1].re;
     var to = group[1].to;
 
@@ -42,15 +77,84 @@ module.exports.makeRe = function makeRe(str, options) {
         to += group[1][key];
       }
     }
-
-    str = str.replace(re, to);
+    glob = glob.replace(re, to);
   }
 
-  if (opts.nocase) f += 'i';
+  if (opts.nocase) flags += 'i';
 
-  var res = /^!/.test(str)
-    ? '^(?!((?!\\.)' + str.slice(1) + ')$)'
-    : '^' + str + '$';
+  var res = /^!/.test(glob)
+    ? '^(?!((?!\\.)' + glob.slice(1) + ')$)'
+    : '^' + glob + '$';
 
-  return new RegExp(res, f);
-};
+  regex = new RegExp(res, flags);
+  return regex;
+}
+
+function equal(a, b) {
+  if (!b) return a;
+  for (var prop in b) {
+    if (!a.hasOwnProperty(prop)) {
+      return false;
+    }
+    if (a[prop] !== b[prop]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function match(files, pattern, options) {
+  if (typeof files !== 'string' && !Array.isArray(files)) {
+    throw new Error('micromatch.match() expects a string or array.');
+  }
+
+  files = arrayify(files);
+  var len = files.length;
+  var res = [];
+  var i = 0;
+
+  while (i < len) {
+    var fp = files[i++];
+    if (makeRe(pattern, options).test(fp)) {
+      res.push(fp);
+    }
+  }
+  return res;
+}
+
+function micromatch(files, patterns, options) {
+  options = options || {};
+
+}
+
+function arrayify(val) {
+  return !Array.isArray(val)
+    ? [val]
+    : val;
+}
+
+/**
+ * Results cache
+ */
+
+var regex;
+var cache;
+var optsCache;
+
+/**
+ * Expose `micromatch`
+ */
+
+module.exports = micromatch;
+
+/**
+ * Expose `micromatch.match`
+ */
+
+module.exports.match = match;
+
+/**
+ * Expose `micromatch.makeRe`
+ */
+
+module.exports.makeRe = makeRe;
