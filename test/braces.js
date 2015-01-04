2dd67761dc8b1fa6d8e34a4fe1a0bb437f6fe0ea
jonschlinkert <jon.schlinkert@sellside.com>
2015-01-04 12:13:11 -0500

    add range test

--- test/braces.js
+++ test/braces.js
@@ -39,4 +39,8 @@ describe('brace expansion', function () {
   it('should expand character classes:', function () {
     mm.match(['aa', 'ab', 'ac', 'ad', 'bad', 'baa', 'bbaa'], '*(a|{b),c)}').should.eql(['aa', 'ab', 'ac', 'baa', 'bbaa']);
   });
+
+  it('should handle range expansion:', function () {
+    mm.match(['aa', 'ab', 'ac', 'acc', 'ad', 'ae', 'af', 'ag'], '*{a..e}').should.eql(['aa', 'ab', 'ac', 'acc', 'ad', 'ae']);
+  });
 });
