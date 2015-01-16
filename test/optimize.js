0d55bcd9b1dc8d3ce182ad4f8fc5b7576c09e77a
jonschlinkert <jon.schlinkert@sellside.com>
2015-01-16 12:18:39 -0500

    add optimization tests

---
+++ test/optimize.js
@@ -0,0 +1,24 @@
+/*!
+ * micromatch <https://github.com/jonschlinkert/micromatch>
+ *
+ * Copyright (c) 2014-2015, Jon Schlinkert.
+ * Licensed under the MIT License
+ */
+
+'use strict';
+
+var path = require('path');
+var should = require('should');
+var argv = require('minimist')(process.argv.slice(2));
+var mm = require('..');
+
+if ('minimatch' in argv) {
+  mm = require('minimatch');
+}
+
+describe('brace expansion', function () {
+  it('should optimize regex when `optimize` is true:', function () {
+    mm.makeRe('{{c,d}..{d,f}}').should.eql(/^(?:c|d|e|f)$/);
+    mm.makeRe('{c,d}/**/{d,f}').should.eql(/^(?:(?:c|d)\/(?:(?!(?:\/|^)\.).)*?\/(?:d|f))$/);
+  });
+});
