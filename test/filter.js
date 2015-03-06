d97f6e5004f04ffc6e47f001c70f9db9ca6c7595
jonschlinkert <jon.schlinkert@sellside.com>
2015-03-06 18:18:39 -0500

    closes https://github.com/jonschlinkert/micromatch/issues/7

--- test/filter.js
+++ test/filter.js
@@ -16,29 +16,45 @@ if ('minimatch' in argv) {
   mm = ref.minimatch;
 }
 
-describe('.filter()', function () {
-  describe('errors:', function () {
-    it('should throw on undefined args:', function () {
-      (function () {
-        mm.filter();
-      }).should.throw('micromatch.filter(): pattern should be a string.');
-    });
+var arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
 
-    it('should throw on bad args:', function () {
-      (function () {
-        mm.filter({});
-      }).should.throw('micromatch.filter(): pattern should be a string.');
-    });
+describe('.filter()', function () {
+  it('should throw on undefined args:', function () {
+    (function () {
+      mm.filter();
+    }).should.throw('micromatch.filter(): patterns should be a string or array.');
   });
+
+  it('should throw on bad args:', function () {
+    (function () {
+      mm.filter({});
+    }).should.throw('micromatch.filter(): patterns should be a string or array.');
+  });
+
   it('should create a filter function to filter files', function () {
     mm.filter('*').should.be.a.function;
   });
 
   it('should filter files', function () {
-    mm.filter('*')(['a', 'b', 'c']).should.eql(['a', 'b', 'c']);
-    mm.filter('a/*')(['a/a', 'b/a', 'a/c']).should.eql(['a/a', 'a/c']);
+    ['a', 'b', 'c'].filter(mm.filter('*')).should.eql(['a', 'b', 'c']);
+    ['a/a', 'b/a', 'a/c'].filter(mm.filter('a/*')).should.eql(['a/a', 'a/c']);
+  });
 
-    (['a', 'b', 'c'].filter(mm.filter('*'))).should.eql(['a', 'b', 'c']);
-    (['a/b', 'a/c', 'b/c'].filter(mm.filter('a/*'))).should.eql(['a/b', 'a/c']);
+  it('should filter using multiple patterns', function () {
+    var actual_A = arr.filter(mm.filter(['{1..10}', '![7-9]', '!{3..4}']));
+    actual_A.should.eql([1, 2, 5, 6, 10]);
+    actual_A.should.not.eql([1, 2, 3, 4, 5, 6, 10]);
+
+    // see https://github.com/jonschlinkert/micromatch/issues/7
+    var actual_B = [
+      'fs-readdir-callback-api.js',
+      'fs-readdir-stream-api.js',
+      'glob-stream.js',
+      'readdirp-callback-api.js',
+      'readdirp-stream-api.js',
+      'recursive-readdir.js'
+    ].filter(mm.filter(['*', '!*api*']));
+    actual_B.should.eql(['glob-stream.js', 'recursive-readdir.js']);
   });
 });
+
