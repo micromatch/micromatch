971ba5c97a4a9a5eeedb9bc7a59eba029d1c3eff
jonschlinkert <jon.schlinkert@sellside.com>
2016-11-24 16:58:36 -0500

    braces examples

--- examples/braces.js
+++ examples/braces.js
@@ -1,7 +1,13 @@
 var mm = require('../');
 
 console.log(mm.braces('{a,b}'));
+//=> [ '(a|b)' ]
+
+console.log(mm.braces('{a,b}', {expand: true}));
 //=> [ 'a', 'b' ]
 
 console.log(mm.braces('foo/{a,b}/bar'));
+//=> [ 'foo/(a|b)/bar' ]
+
+console.log(mm.braces('foo/{a,b}/bar', {expand: true}));
 //=> [ 'foo/a/bar', 'foo/b/bar' ]
