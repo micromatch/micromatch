5974ea9fbe86771e77c82fae64a07c2e1c67437b
jonschlinkert <jon.schlinkert@sellside.com>
2017-06-02 05:00:20 -0400

    lint

--- test/minimatch.js
+++ test/minimatch.js
@@ -53,7 +53,7 @@ describe('minimatch parity:', function() {
   describe('minimatch issues (as of 12/7/2016)', function() {
     it('https://github.com/isaacs/minimatch/issues/29', function() {
       assert(mm.isMatch('foo/bar.txt', 'foo/**/*.txt'));
-      assert(mm.makeRe('foo/**/*.txt' ).test( 'foo/bar.txt'));
+      assert(mm.makeRe('foo/**/*.txt').test('foo/bar.txt'));
       assert(!mm.isMatch('n/!(axios)/**', 'n/axios/a.js'));
       assert(!mm.makeRe('n/!(axios)/**').test('n/axios/a.js'));
     });
@@ -63,7 +63,7 @@ describe('minimatch parity:', function() {
       assert(mm.isMatch('./foo/bar.js', './**/foo/**'));
       assert(mm.isMatch('./foo/bar.js', '**/foo/**'));
       assert(mm.isMatch('./foo/bar.txt', 'foo/**/*.txt'));
-      assert(mm.makeRe('./foo/**/*.txt' ).test( 'foo/bar.txt'));
+      assert(mm.makeRe('./foo/**/*.txt').test('foo/bar.txt'));
       assert(!mm.isMatch('./foo/!(bar)/**', 'foo/bar/a.js'));
       assert(!mm.makeRe('./foo/!(bar)/**').test('foo/bar/a.js'));
     });
